8b8b67a455e6fd360b43df14052e61248e6fa827
Arslan <arslan_bazarbaev@mail.ru>
2020-01-13 14:14:51 +0500

    Доработка выбора поля Даты в Заказах. Запрет выбора будущих дат.

--- modules/web/src/com/company/mimismart/web/screens/order_from_supplier/Order_from_supplierEdit.java
+++ modules/web/src/com/company/mimismart/web/screens/order_from_supplier/Order_from_supplierEdit.java
@@ -1,7 +1,12 @@
 package com.company.mimismart.web.screens.order_from_supplier;
 
+import com.haulmont.cuba.gui.components.DateField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.mimismart.entity.Order_from_supplier;
+
+import javax.inject.Inject;
+import java.util.Date;
+
 
 @UiController("mimismart_Order_from_supplier.edit")
 @UiDescriptor("order_from_supplier-edit.xml")
@@ -9,4 +14,12 @@ import com.company.mimismart.entity.Order_from_supplier;
 @LoadDataBeforeShow
 public class Order_from_supplierEdit extends StandardEditor<Order_from_supplier> {
 
+    @Inject
+    private DateField<Date> dateField;
+
+    @Subscribe
+    public void onInit(InitEvent event) {
+        dateField.setRangeEnd(new Date());
+    }
+        
 }
