267a5f17dae51928e8f5929898c0a5ea6690bb12
Arslan <arslan_bazarbaev@mail.ru>
2020-01-19 00:56:50 +0500

    Доработка выбора поля Даты в Заказах. Запрет выбора будущих дат.

--- modules/web/src/com/company/mimismart/web/screens/order_from_supplier/Order_from_supplierEdit.java
+++ modules/web/src/com/company/mimismart/web/screens/order_from_supplier/Order_from_supplierEdit.java
@@ -1,15 +1,22 @@
 package com.company.mimismart.web.screens.order_from_supplier;
 
 import com.company.mimismart.entity.OrderLine;
-import com.haulmont.cuba.gui.actions.list.EditAction;
+import com.haulmont.cuba.core.global.Metadata;
+import com.haulmont.cuba.gui.components.Action;
+import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.components.DateField;
-import com.haulmont.cuba.gui.components.DatePicker;
+import com.haulmont.cuba.gui.components.actions.BaseAction;
+import com.haulmont.cuba.gui.data.CollectionDatasource;
+import com.haulmont.cuba.gui.model.CollectionPropertyContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.mimismart.entity.Order_from_supplier;
+import com.sun.istack.Nullable;
 
 import javax.inject.Inject;
 import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
 
 
 @UiController("mimismart_Order_from_supplier.edit")
@@ -22,12 +29,28 @@ public class Order_from_supplierEdit extends StandardEditor<Order_from_supplier>
     private DateField<Date> dateField;
     @Inject
     private DataGrid<OrderLine> order_from_supplierOrderlinesDataGrid;
+    @Inject
+    private Metadata metadata;
+    @Inject
+    private CollectionPropertyContainer<OrderLine> order_from_supplierOrderlinesesDc;
+    @Inject
+    private Button myButton;
+
 
     @Subscribe
     public void onInit(InitEvent event) {
         dateField.setRangeEnd(new Date());
-        order_from_supplierOrderlinesDataGrid.getColumnNN("date");
     }
-    
-        
+
+    @Nullable
+    public void init(Map<String, Object> params) {
+        Action addAction = new BaseAction("addOrderLine").withHandler(actionPerformedEvent -> {
+            OrderLine orderline = metadata.create(OrderLine.class);
+            order_from_supplierOrderlinesesDc.getItems().add(orderline);
+
+            order_from_supplierOrderlinesDataGrid.edit(orderline);
+        });
+        myButton.setAction(addAction);
+    }
+
 }
