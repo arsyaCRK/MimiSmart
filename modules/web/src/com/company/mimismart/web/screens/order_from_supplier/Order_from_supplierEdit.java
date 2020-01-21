7c6631395251dfcf342f76dc181148d289a8aea5
Arslan <arslan_bazarbaev@mail.ru>
2020-01-21 10:40:49 +0500

    В заказах сделано как в 1С при добавлении состава норвого заказа.

--- modules/web/src/com/company/mimismart/web/screens/order_from_supplier/Order_from_supplierEdit.java
+++ modules/web/src/com/company/mimismart/web/screens/order_from_supplier/Order_from_supplierEdit.java
@@ -1,22 +1,16 @@
 package com.company.mimismart.web.screens.order_from_supplier;
 
 import com.company.mimismart.entity.OrderLine;
-import com.haulmont.cuba.core.global.Metadata;
 import com.haulmont.cuba.gui.components.Action;
-import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.DataGrid;
 import com.haulmont.cuba.gui.components.DateField;
-import com.haulmont.cuba.gui.components.actions.BaseAction;
-import com.haulmont.cuba.gui.data.CollectionDatasource;
 import com.haulmont.cuba.gui.model.CollectionPropertyContainer;
+import com.haulmont.cuba.gui.model.DataContext;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.mimismart.entity.Order_from_supplier;
-import com.sun.istack.Nullable;
 
 import javax.inject.Inject;
 import java.util.Date;
-import java.util.Map;
-import java.util.UUID;
 
 
 @UiController("mimismart_Order_from_supplier.edit")
@@ -30,27 +24,31 @@ public class Order_from_supplierEdit extends StandardEditor<Order_from_supplier>
     @Inject
     private DataGrid<OrderLine> order_from_supplierOrderlinesDataGrid;
     @Inject
-    private Metadata metadata;
-    @Inject
     private CollectionPropertyContainer<OrderLine> order_from_supplierOrderlinesesDc;
     @Inject
-    private Button myButton;
+    private DataContext dataContext;
 
 
     @Subscribe
     public void onInit(InitEvent event) {
+
         dateField.setRangeEnd(new Date());
     }
 
-    @Nullable
-    public void init(Map<String, Object> params) {
-        Action addAction = new BaseAction("addOrderLine").withHandler(actionPerformedEvent -> {
-            OrderLine orderline = metadata.create(OrderLine.class);
-            order_from_supplierOrderlinesesDc.getItems().add(orderline);
+    @Subscribe("order_from_supplierOrderlinesDataGrid.create")
+    public void onOrdersDataGridCreate(Action.ActionPerformedEvent event) {
+        OrderLine orderline = dataContext.create(OrderLine.class);
+        orderline.setOrder(getEditedEntity());
+        order_from_supplierOrderlinesesDc.getMutableItems().add(orderline);
+        order_from_supplierOrderlinesDataGrid.edit(orderline);
+    }
 
-            order_from_supplierOrderlinesDataGrid.edit(orderline);
-        });
-        myButton.setAction(addAction);
+    @Subscribe("order_from_supplierOrderlinesDataGrid.edit")
+    public void onOrdersDataGridEdit(Action.ActionPerformedEvent event) {
+        OrderLine orderline = dataContext.create(OrderLine.class);
+        orderline.setOrder(getEditedEntity());
+        order_from_supplierOrderlinesesDc.getMutableItems().add(orderline);
+        order_from_supplierOrderlinesDataGrid.edit(orderline);
     }
 
 }
